fabdfebe7fa54d2b345ddc571c0e6ff372fbf495
wt195799611 <87539769+wt195799611@users.noreply.github.com>
2024-09-26 14:29:24 +0800

    Update update_node.sh

--- update_node.sh
+++ update_node.sh
@@ -11,8 +11,10 @@ echo "正在杀死所有 nillion_monitor 会话..."
 SESSION_IDS=$(screen -ls | grep 'nillion_monitor' | awk '{print $1}' | cut -d '.' -f 1)
 
 if [[ -n "$SESSION_IDS" ]]; then
-    echo "$SESSION_IDS" | xargs kill
-    echo "已杀死所有 nillion_monitor 会话。"
+    echo "找到的 nillion_monitor 会话ID: $SESSION_IDS"
+    
+    # 尝试杀死会话并检查退出状态
+    echo "$SESSION_IDS" | xargs -I{} kill -9 {} && echo "已成功杀死会话: $SESSION_IDS" || echo "杀死会话失败。"
 else
     echo "没有找到 nillion_monitor 会话。"
 fi
@@ -26,6 +28,6 @@ chmod +x node.sh
 
 # 创建新的 screen 会话并在后台运行新脚本
 echo "正在创建新的 screen 会话并运行脚本..."
-screen -S nillion_monitor ./node.sh
+screen -dmS nillion_monitor ./node.sh
 
 echo "脚本已更新并在新的 screen 会话中运行。"
